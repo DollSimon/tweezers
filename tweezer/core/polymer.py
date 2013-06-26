8134aeb219ac70ce5e977012de642c12c9e22e55
Marcus Jahnel <jahnel@mpi-cbg.de>
2013-06-26 14:09:06 +0200

    ENH: rework function definition for nice formatting in ipython notebook and numpy broadcasting

--- tweezer/core/polymer.py
+++ tweezer/core/polymer.py
@@ -1,13 +1,18 @@
+from __future__ import print_function, division
+
 """
 Polymer
 --------
 
-An abstraction representation of semi-flexible polymers and \
+A representation of semi-flexible polymers and \
 corresponding classes.
 """
+import numpy as np
+import pandas as pd
+
 from scipy.constants import Boltzmann
 from math import sqrt
-from numpy import nan
+
 
 
 class ExtensibleWormLikeChain(object):
@@ -18,12 +23,14 @@ class ExtensibleWormLikeChain(object):
     X(F; L, Lp, S) = L(1 - 1/2*sqrt(kT/F*Lp) + F/S)
 
     Parameter:
+    ----------
         L - contour_length
         P - persistence_length (50 nm)
         S - stretch_modulus (1200 pN)
         T - temperature (295 K)
 
     Methods:
+    ---------
         constructor(L, P, S, T): set parameter (with default values)
         extension(force): compute the extension at a certain force
         formula(): print out formula used
@@ -32,38 +39,51 @@ class ExtensibleWormLikeChain(object):
     def __init__(self, contour_length, persistence_length=50,
                  stretch_modulus=1200, temperature=295):
 
-        self.S = float(stretch_modulus)
-        self.P = float(persistence_length)
-        self.L = float(contour_length)
-        self.T = float(temperature)
+        self.S = np.float64(stretch_modulus)
+        self.P = np.float64(persistence_length)
+        self.L = np.float64(contour_length)
+        self.T = np.float64(temperature)
+        self.Boltzmann = Boltzmann
+        self.kBT = self.T * self.Boltzmann * 1e21 # kBT in units of pN * nm
+        self.units = {'P': 'nm', 'T': 'K', 'L': 'nm', 'S': 'pN', 'Boltzmann': 'J / K', 'kBT': 'pN nm'}
 
-    def __call__(self, force):
-        kB = Boltzmann
-        f = float(force)
-
-        if f >= 0:
-            x = self.L * (1 - (1 / 2.0) * sqrt((kB * self.T) / (f * self.P)) +
-                         (f / self.S))
+    def extension(self, force, **kwargs):
+        if not kwargs:
+            contour_length = self.L
+            persistence_length = self.P
+            stretch_modulus = self.S
         else:
-            x = nan
-
-        return x
-
-    def extension(self, force):
-        kB = Boltzmann
-        f = float(force)
+            contour_length = kwargs.get('L', self.L)
+            persistence_length = kwargs.get('P', self.P)
+            stretch_modulus = kwargs.get('S', self.S)
+            
+        f = np.float64(force)
 
         if f >= 0:
-            x = self.L * (1 - (1 / 2.0) * sqrt((kB * self.T) / (f * self.P)) +
-                         (f / self.S))
+            x = contour_length * (1 - (1 / 2.0) * sqrt((self.kBT) / (f * persistence_length)) +
+                         (f / stretch_modulus))
         else:
             raise(ValueError("Force must be positive"))
-            x = ValueError("Rocket")
 
         return x
 
+    def __call__(self, force, **kwargs):
+        return np.vectorize(self.extension)(force, **kwargs)
+
+    def _repr_latex_(self):
+        return r'$X_{\mathrm{eWLC}}(F\, ; L_{\mathrm{C}}\, , P\, , S\, ) = L_{\mathrm{C}}\big( 1 - \frac{1}{2}\sqrt{\frac{k_{\mathrm{B}} T}{P\,\cdot\, F}} + \frac{F}{S}\big)$'
+
     def formula(self):
-        return "X(F; L, Lp, S) = L(1 - 1/2 * sqrt(kT/F*Lp) + F/S)"
+        L = self.L
+        P = self.P
+        S = self.S
+        return "X(F; L={L}, Lp={P}, S={S}) = L(1 - 1/2 * sqrt(kT/F*Lp) + F/S)".format(L=L, P=P, S=S)
+
+    def __repr__(self):
+        L = self.L
+        P = self.P
+        S = self.S
+        return "X(F; L={L}, Lp={P}, S={S}) = L(1 - 1/2 * sqrt(kT/F*Lp) + F/S)".format(L=L, P=P, S=S)
 
 
 class LengthDependentPersistenceLength(object):
