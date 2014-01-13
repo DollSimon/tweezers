64ca93583a7fbac1a126fb72e0a2054dbfd1f266
Marcus Jahnel <jahnel@mpi-cbg.de>
2014-01-13 14:22:27 +0100

    CLN/TST: fix test of viscosity algorithm to compare two floats are almost equal

--- tests/test_ixo_viscosity.py
+++ tests/test_ixo_viscosity.py
@@ -2,20 +2,14 @@
 #-*- coding: utf-8 -*-
 import pytest
 
+from numpy.testing import assert_almost_equal as aae
+
 from tweezer.ixo.viscosity import dynamic_viscosity_of_mixture
 
 
-@pytest.mark.wip
-# @pytest.mark.parametrize("W, G, T, expected", [
-#     ("0.4", "2.3", "25", 0.11667),
-#     ("3", "3", "25", 0.0068792),
-#     ("0", "3", "25", 0.90568),
-#     ("4", "3", "5", 0.010586),
-#     ("3", "0", "25", 0.00089274)])
-# def test_viscosity_of_known_solutions(W, G, T, expected):
-#     assert dynamic_viscosity_of_mixture(W, G, T) == expected
+@pytest.mark.algorithm
 def test_viscosity_calculation():
-    # assert dynamic_viscosity_of_mixture(3, 3, 25) == 0.0068792
-    # assert dynamic_viscosity_of_mixture(3, 0, 25) == 0.00089274
-    assert dynamic_viscosity_of_mixture(0, 3, 25) == 0.90568
-    assert dynamic_viscosity_of_mixture(4, 3, 5) == 0.010586
+    aae(dynamic_viscosity_of_mixture(3, 3, 25), 0.0068792)
+    aae(dynamic_viscosity_of_mixture(3, 0, 25), 0.00089274)
+    aae(dynamic_viscosity_of_mixture(0, 3, 25), 0.90568)
+    aae(dynamic_viscosity_of_mixture(4, 3, 5), 0.010586, 6)
