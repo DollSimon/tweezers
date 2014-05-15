f858690a7a4bde7dddf4093a2ecfc7b0c72b0520
Marcus Jahnel <jahnel@mpi-cbg.de>
2014-05-15 23:57:53 +0200

    ENH: explicit is better than implicit; add documentation and verbosity flag; TODO: find a way to set this flag globally for many functions

--- tweezer/physics/hydrodynamics.py
+++ tweezer/physics/hydrodynamics.py
@@ -4,14 +4,21 @@
 import numpy as np
 
 
-def drag_on_sphere(radius=1000, viscosity=0.9e-9):
+def drag_on_sphere(radius=1000, dynamicViscosity=0.9e-9, verbose=False):
     """
     Calculates the simple Stokes' drag coefficient of a sphere in a Newtonian fluid
+    at low Reynolds number.
 
-    :param radius: (number) radius of solid sphere in [nm]
-    :param viscosity: (number) dynamic viscosity in [pN/nm^2 s]
+    :param radius: radius of solid sphere in [nm]; default: 1000
+    :type radius: float
 
-    :return drag_coefficent: description
+    :param dynamicViscosity: dynamic viscosity in [pN/nm^2 s]; default: 0.9e-9
+    :type dynamicViscosity: float
+
+    :param verbose: print parameters and results with units; default: False
+
+    :return dragCoefficient: Stokes drag coefficient in [pN/nm s]
+    :rtype: float
     """
     if not isinstance(radius, (int, float, np.float)):
         try:
@@ -19,16 +26,25 @@ def drag_on_sphere(radius=1000, viscosity=0.9e-9):
         except ValueError:
             print('Radius must be a number, not a {}'.format(type(radius)))
 
-    if not isinstance(viscosity, (int, float, np.float)):
+    if not isinstance(dynamicViscosity, (int, float, np.float)):
         try:
-            viscosity = np.float(viscosity)
+            dynamicViscosity = np.float(dynamicViscosity)
         except ValueError:
-            print('Viscosity must be a number, not a {}'.format(type(viscosity)))
+            print('Viscosity must be a number, not a {}'.format(type(dynamicViscosity)))
 
     assert (radius > 0), 'Radius of sphere must be positive'
 
-    drag_coefficient = 6 * np.pi * radius * viscosity
-    return drag_coefficient
+    dragCoefficient = 6 * np.pi * radius * dynamicViscosity
+
+    if verbose:
+        print("In:")
+        print("Radius: r = {} nm".format(radius))
+        print("Viscosity: eta = {} pN/nm^2 s\n".format(dynamicViscosity))
+
+        print("Out:")
+        print("Drag coefficient: gamma = {} pN/nm s".format(round(dragCoefficient, 12)))
+
+    return dragCoefficient
 
 
 class StokesDragSphere(object):
