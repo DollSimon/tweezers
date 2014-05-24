024a792fd3ad64d6fefb5e2ce48048e574840d8d
Marcus Jahnel <jahnel@mpi-cbg.de>
2014-05-24 02:00:40 +0200

    DEV: adding theoretical predictions for Allan variance calculations for single traps

--- tweezer/noise/allan.py
+++ tweezer/noise/allan.py
@@ -4,6 +4,14 @@ import pandas as pd
 from numpy import (log10, zeros, sqrt, floor, ceil, array, mean, sum, diff, empty, float)
 from numba import jit
 
+try:
+    from tweezer.physics import thermal_energy
+except ImportError as err:
+    print('The tweezer package has not been correctly installed or updated.')
+    raise err
+
+
+# Calculations of actual time series ----
 
 def allan_variance_r_port(data, frequency):
     """
@@ -53,38 +61,6 @@ def allan_variance_r_port(data, frequency):
     return allanVariances
 
 
-def allan_variance(data, dt=1):
-    """
-    Compute the Allan variance on a set of regularly-sampled data (1D).
-
-       If the time between samples is dt and there are N total
-       samples, the returned variance spectrum will have frequency
-       indices from 1/dt to (N-1)/dt.
-
-    """
-    # 2008-07-30 10:20 IJC: Created
-    # 2011-04-08 11:48 IJC: Moved to analysis.py
-    # 2011-10-27 09:25 IJMC: Corrected formula; thanks to Xunchen Liu
-    #                        of U. Alberta for catching this.
-
-    newdata = array(data, subok=True, copy=True)
-    dsh = newdata.shape
-
-    newdata = newdata.ravel()
-
-    nsh = newdata.shape
-
-    alvar = zeros(nsh[0]-1, float)
-
-    for lag in range(1, nsh[0]):
-        # Old, wrong formula:
-        #alvar[lag-1]  = mean( (newdata[0:-lag] - newdata[lag:])**2 )
-        alvar[lag-1] = (mean(newdata[0:(lag+1)])-mean(newdata[0:lag]))**2
-
-    return (alvar*0.5)
-
-
-
 @jit('f8(f8[:], f8[:], uint8, uint16)')
 def core_allan(data, D, M, N):
     """
@@ -106,11 +82,6 @@ def core_allan(data, D, M, N):
 
     :rtype: float
     """
-    # print("Core")
-    # print("N: {}".format(N))
-    # print("M: {}".format(M))
-    # print("N - M + 1: {}".format(N - M + 1))
-    # print("D.shape: {}".format(D.shape))
     D[0] = sum(data[0:M]) / M
 
     for i in range(1, N - M + 1):
@@ -120,39 +91,92 @@ def core_allan(data, D, M, N):
 
 
 @jit
-def allan_variances(inData, tau, rate):
+def allan_variance(data, rate):
     """
     Calculates the Allan variance of a time series.
 
+    .. todo: type checking and better documentation
+    .. todo: add different versions of calculating tau values (overlapping,
+    non-overlapping, etc.)
     """
-    N = inData.shape[0]
+    tau = generate_allan_tau(data=data, rate=rate)
+
+    N = data.shape[0]
     J = tau.shape[0]
     A = empty(J, dtype=float)
     D = empty(N, dtype=float)
     M = floor(tau * rate).astype(int)
 
-    # print("N: {}".format(N))
-    # print("J: {}".format(J))
-    # print("M: {}".format(M))
-    # print("len(M): {}".format(len(M)))
-    # print("A.shape[0]: {}".format(A.shape[0]))
-
     for j in range(J):
 
-        A[j] = core_allan(inData, D, M[j], N)
+        A[j] = core_allan(data, D, M[j], N)
 
     return A
 
 
-def generate_allan_tau(inData, rate):
-
+def generate_allan_tau(data, rate):
+    """
+    Generates tau, or time lag, values for Allan variance calculations.
+    """
     firstTau = 1 / rate
 
-    N = inData.shape[0]
+    N = data.shape[0]
     n = int(ceil((N - 1) / 2))
     p = int(floor(log10(n) / log10(2)))
 
     tau = [(2**i) * firstTau for i in range(p + 1)]
 
     return array(tau)
+
+
+# Theoretical results ----
+
+def _allan_shape_factor(N=4096, m=8):
+    """
+
+    :param N: sample size
+    :type N: int
+
+    :param m: block size
+    :type m: int
+    """
+
+    N = nSamples
+    m = blockSize
+
+    return (1 / 2) * ((N / m) - 1)
+
+
+def allan_variance_single_trap_thermal_limit(tau,
+                               gamma = 4E-5,
+                               k = 2E-4,
+                               kBT = thermal_energy(),
+                               asDataFrame = True):
+    """
+    Theoretical limit of Allan variance of a bead in an optical trap.
+
+    .. math::
+
+        \sigma^2(\\tau) = 2 k_{B} T \gamma / \kappa^2 \\tau
+
+    """
+    avar = 2 * kBT * gamma / (k**2 * tau)
+
+    if asDataFrame:
+        result = pd.DataFrame({"tau": tau, "avar": avar})
+    else:
+        result = avar
+
+    return result
+
+
+def allan_variance_single_trap(tau,
+                               gamma = 4E-5,
+                               k = 2E-4,
+                               kBT = thermal_energy(),
+                               asDataFrame = True):
+    pass
+
+
+
 
