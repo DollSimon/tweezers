3c92c14e1b5a2fc129c66fc516aaf211da552311
Marcus Jahnel <jahnel@mpi-cbg.de>
2014-07-11 12:46:43 +0200

    ENH/DOC: making use of UNICODE in PY3; great to have greek variable names and superscripts in IPython

--- tweezer/physics/utils.py
+++ tweezer/physics/utils.py
@@ -45,7 +45,7 @@ def volume_sphere(radius:float = 1000):
         radius (float): Radius in [nm]. (Default: 1000 nm)
 
     Returns:
-        volume (float): Volume in [nm^3]
+        volume (float): Volume in [nm³]
     """
     assert radius > 0
 
@@ -61,9 +61,9 @@ def mass_sphere(radius:float = 1000, density:float = 1e-21,
 
     Args:
         radius (float): Radius in [nm]. (Default: 1000 nm)
-        density (float): Density, ϱ, in [g / nm^3]. (Default: 1e-21 g/nm^3)
+        density (float): Density, ϱ, in [g/nm³]. (Default: 1e-21 g/nm³)
         verbose (bool): Flag whether to show extra information. (Default: False)
-        inferDensity (bool): If true inputs above 1e-5 are interpreted as g/cm^3. (Default: True)
+        inferDensity (bool): If true inputs above 1e-5 are interpreted as g/cm³. (Default: True)
 
     Returns:
         mass (float):  Mass in [g]
@@ -73,18 +73,18 @@ def mass_sphere(radius:float = 1000, density:float = 1e-21,
 
     volume = volume_sphere(radius = radius)
 
-    # check if density is provided in g/cm^3
+    # check if density is provided in g/cm³.
     if inferDensity and density > 1e-5:
-        print("Assuming that ϱ was provided in [g/cm^3]. Converting to g/nm^3")
+        print("Assuming that ϱ was provided in [g/cm³]. Converting to g/nm³")
         density /= 1e21
-        print("ϱ is now: {} g/nm^3".format(density))
+        print("ϱ is now: {} g/nm³".format(density))
 
     mass = volume * density
 
     if verbose:
         print("Input:")
         print("Radius of Sphere [nm]: {}".format(radius))
-        print("Density of material [g / nm^3]: {} \n".format(density))
+        print("Density of material [g/nm³]: {} \n".format(density))
 
         print("Output:")
         print("Mass of sphere [g]: {}\n".format(mass))
