0a2d83b540d029bbafd4f9c3708385394045a651
Marcus Jahnel <jahnel@mpi-cbg.de>
2014-07-11 12:37:07 +0200

    DOC: adjust some documentation to style guide; add density inference to mass fnc

--- tweezer/physics/utils.py
+++ tweezer/physics/utils.py
@@ -7,16 +7,11 @@ def as_Kelvin(temperatureInCelsius=25):
     """
     Converts temperature from Celsius to Kelvin
 
-    Parameter
-    ---------
-    temperatureInCelsius : float
-        Temperature in degree Celsius
-        Default: 25 C
+    Args:
+        temperatureInCelsius (float): Temperature in [°C]. (Default: 25 °C)
 
-    Returns
-    -------
-    temperatureInKelvin : float
-        Temperature in Kelvin
+    Returns:
+        temperatureInKelvin (float): Temperature in Kelvin [K]
     """
     assert temperatureInCelsius >= -273.15
 
@@ -29,16 +24,11 @@ def as_Celsius(temperatureInKelvin=298):
     """
     Converts temperature from Kelvin to Celsius
 
-    Parameter
-    ---------
-    temperatureInKelvin : float
-        Temperature in Kelvin
-        Default: 298 K
+    Args:
+        temperatureInKelvin (float): Temperature in Kelvin [K]. (Default: 298 K)
 
-    Returns
-    -------
-    temperatureInCelsius: float
-        Temperature in degree Celsius
+    Returns:
+        temperatureInCelsius (float): Temperature in [°C]. (Default: 25 °C)
     """
     assert temperatureInKelvin >= 0
 
@@ -47,20 +37,15 @@ def as_Celsius(temperatureInKelvin=298):
     return temperatureInCelsius
 
 
-def volume_sphere(radius=1000):
+def volume_sphere(radius:float = 1000):
     """
     Volume of a sphere
 
-    Parameters
-    ----------
-    radius : float
-        Radius in [nm]
-        Default: 1000
+    Args:
+        radius (float): Radius in [nm]. (Default: 1000 nm)
 
-    Returns
-    -------
-    volume : float
-        Volume in [nm^3]
+    Returns:
+        volume (float): Volume in [nm^3]
     """
     assert radius > 0
 
@@ -69,28 +54,30 @@ def volume_sphere(radius=1000):
     return volume
 
 
-def mass_sphere(radius=1000, density=1e-21, verbose=False):
+def mass_sphere(radius:float = 1000, density:float = 1e-21,
+                verbose:bool = False, inferDensity:bool = True) -> float:
     """
     Calculates mass of a sphere
 
-    Parameters
-    ----------
-    radius : float
-        Radius in [nm]
-        Default: 1000
+    Args:
+        radius (float): Radius in [nm]. (Default: 1000 nm)
+        density (float): Density, ϱ, in [g / nm^3]. (Default: 1e-21 g/nm^3)
+        verbose (bool): Flag whether to show extra information. (Default: False)
+        inferDensity (bool): If true inputs above 1e-5 are interpreted as g/cm^3. (Default: True)
 
-    density : float
-        Density in [g / nm^3]
-
-    Returns
-    -------
-    mass : float
-        Mass in [g]
+    Returns:
+        mass (float):  Mass in [g]
     """
     assert radius > 0
     assert density > 0
 
     volume = volume_sphere(radius = radius)
+
+    # check if density is provided in g/cm^3
+    if inferDensity and density > 1e-5:
+        print("Assuming that ϱ was provided in [g/cm^3]. Converting to g/nm^3")
+        density /= 1e21
+        print("ϱ is now: {} g/nm^3".format(density))
 
     mass = volume * density
 
