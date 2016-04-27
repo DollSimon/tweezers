FIX: force calculation, tweezers data container added, maintenance stuff, bug fixes

tweezers/analysis/psd.py
@@ -5,7 +5,7 @@ import numpy as np
 from collections import OrderedDict
 
 
-class PsdComputation():
+class PsdComputation:
     """
     Object to compute a PSD using Welch's method (:func:`scipy.signal.welch`).
     """
@@ -60,6 +60,10 @@ class PsdComputation():
 
         psd = pd.DataFrame()
         for title, column in self.timeSeries.items():
+            # ignore time column if present
+            if title in ['t']:
+                continue
+
             psdRaw = self.computePsd(column,
                                      samplingFreq=self.samplingRate,
                                      blockLength=self.blockLength,
@@ -243,7 +247,7 @@ class PsdFitMle(tweezers.ixo.fit.Fit):
         return errorsMle
 
 
-class PsdFit():
+class PsdFit:
     """
     Fit the PSD.
     """
@@ -288,8 +292,10 @@ class PsdFit():
         returned meta data can describe different things depending on the fitting class used.
 
         Returns:
-            fitParams (dict): fit parameters and metadata as dictionary
-            psdFit (:class:`pandas.DataFrame`): data of the fitted curve
+             2-element tuple containing
+
+                - **fitParams** (:class:`dict`): fit parameters and metadata as dictionary
+                - **psdFit** (:class:`pandas.DataFrame`): data of the fitted curve
         """
 
         # check for frequency column

tweezers/ixo/fit.py
@@ -26,11 +26,18 @@ class Fit():
         self.fcn = fcn
         self.std = std
         self.kwargs = kwargs
-        self.fitResult = []
         self.fitError = []
 
     @lazy
-    def yFit(self):
+    def fitResult(self):
+        """
+        Attribute to hold the computed fitting parameters.
+        """
+
+        return self.fit()
+
+    @lazy
+    def fitY(self):
         """
         Attribute to hold the y values of the fitted curve. Evaluated lazily.
 
@@ -63,8 +70,8 @@ class Fit():
             :numpy:`np.array` and :class:`float`
         """
 
-        residuals = (self.y - self.yFit) / self.yFit
-        meanResidual = np.sum(residuals) / len(self.yFit)
+        residuals = (self.y - self.fitY) / self.fitY
+        meanResidual = np.sum(residuals) / len(self.fitY)
         return residuals, meanResidual
 
     def chisquared(self):
@@ -78,7 +85,7 @@ class Fit():
         if self.std is None or not any(self.std):
             raise AttributeError('No standard deviation data given for χ² computation.')
 
-        chi2 = np.sum((self.y - self.yFit)**2 / self.std**2) / len(self.x)
+        chi2 = np.sum((self.y - self.fitY)**2 / self.std**2) / len(self.x)
         return chi2
 
 
@@ -117,12 +124,12 @@ class LeastSquaresFit(Fit):
             std = None
 
         # perform fit
-        self.fitResult, cov = curve_fit(self.fcn, self.x, self.y,
-                                        p0=[1, 20],
-                                        sigma=std,
-                                        absolute_sigma=True,
-                                        **self.kwargs)
+        res, cov = curve_fit(self.fcn, self.x, self.y,
+                                p0=[1, 20],
+                                sigma=std,
+                                absolute_sigma=True,
+                                **self.kwargs)
         # one standard deviation errors of the fitting parameters, only makes sense with weighted data points
         self.fitError = np.sqrt(np.diag(cov))
 
-        return self.fitResult
+        return res

tweezers/physics/tweezers.py
@@ -24,7 +24,7 @@ def trapStiffness(fc=500, radius=1000, viscosity=8.93e-10):
 
 def distanceCalibration(D=0.46, radius=1000, viscosity=8.93e-10, T=25):
     """
-    Distance calibration factor (beta) in units of [V/nm]
+    Distance calibration factor (beta) in units of [nm/V]
 
     Args:
         D (float): diffusion constant in units of [V]
@@ -36,7 +36,7 @@ def distanceCalibration(D=0.46, radius=1000, viscosity=8.93e-10, T=25):
         :class:`float`
     """
 
-    beta = 1/np.sqrt(thermalEnergy(asKelvin(T))/(dragSphere(radius, viscosity)*D))
+    beta = np.sqrt(thermalEnergy(asKelvin(T))/(dragSphere(radius, viscosity)*D))
 
     return beta
     