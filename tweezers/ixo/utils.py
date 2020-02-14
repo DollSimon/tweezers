5f049c57118d2c1ca82f214fd718d71a50a098f9
Christoph Ehrlich <christoph.ehrlich@tu-dresden.de>
2020-02-14 17:47:50 +0100

    ENH: Added progress bar for Jupyter Notebook

--- tweezers/ixo/utils.py
+++ tweezers/ixo/utils.py
@@ -1,5 +1,6 @@
 import logging as log
 from IPython.display import clear_output
+from datetime import datetime
 
 
 def strToBool(value):
@@ -66,3 +67,60 @@ def nbUpdateProgress(progress):
     clear_output(wait=True)
     text = "Progress: [{0}] {1:.1f}%".format("#" * block + "-" * (bar_length - block), progress * 100)
     print(text)
+
+
+class NbProgress:
+    """
+    A class to provide a progress bar in a Jupyter Notebook.
+    """
+
+    n = None
+    startTime = None
+    barLength = 20
+
+    def __init__(self, n):
+        """
+        Initialize the object. Pass the maximum number to which to count to `n`.
+
+        Args:
+            n: maximum number to count to
+        """
+
+        self.n = n
+        self.startTime = datetime.now()
+
+    def update(self, i):
+        """
+        Update the progress bar. Give the current number `i`. Progress is calculated as `i / n`.
+
+        Returns:
+            i: current number
+        """
+
+        progress = i / self.n
+
+        # make sure we have a number in the right range
+        if isinstance(progress, int):
+            progress = float(progress)
+        if not isinstance(progress, float):
+            progress = 0
+        if progress < 0:
+            progress = 0
+        if progress >= 1:
+            progress = 1
+
+        # get block length
+        block = int(round(self.barLength * progress))
+
+        # get estimated remaining time
+        timeDiff = datetime.now() - self.startTime
+        timeLeft = timeDiff / progress - timeDiff
+        # format a string without the microseconds
+        timeLeftString = str(timeLeft).split('.')[0] + ' h'
+
+        # print output
+        clear_output(wait=True)
+        text = 'Progress: [{0}] {1:.1f}%'.format('#' * block + '-' * (self.barLength - block), progress * 100)
+        text += '\nRemaining Time: {}'.format(timeLeftString)
+
+        print(text)
